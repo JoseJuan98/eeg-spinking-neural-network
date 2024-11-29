666ce6a105e5f8d834f7e8cea8c5ff6418510104
JoseJuan98 <josejuan-98@hotmail.com>
2024-11-29 08:47:19 +0100

    initial commit

---
+++ src/common/__version__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""common package metadata"""
+__title__ = "common"
+__description__ = "Common utilities."
+__version__ = "0.1.0"
+__author__ = "Jose Pena; Felipe Macias; Andreas Pahlsson; Adam Epstein"
+__license__ = "MIT"
+__url__ = "https://github.com/JoseJuan98/d7043E-stock-price-prediction"
